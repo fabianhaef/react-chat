419579dece0d254fca2eb5c64a685da967b094de
fabianhaef <fabian.haefi@gmail.com>
2023-09-10 13:48:43 +0200

    setup chatengine io, create components for chatFeed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChatFeed from './components/ChatFeed';
+
+import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="ae1b1a06-35a5-4c79-bf05-37a938c0db3f"
+            userName="FabianHaef"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    )
 }
 
 export default App;
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,14 @@
+
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+const ChatFeed = (props) => {
+    console.log(props);
+
+    return (
+        <div>ChatFeed</div>
+    )
+}
+
+export default ChatFeed;
