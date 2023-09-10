4ba886ac4702cdf1d381bc5b7ae636bd6c6e1cef
fabianhaef <fabian.haefi@gmail.com>
2023-09-10 15:17:47 +0200

    replace static user credentials with credentials from local storage

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => {
         <ChatEngine
             height="100vh"
             projectID="ae1b1a06-35a5-4c79-bf05-37a938c0db3f"
-            userName="FabianHaef"
-            userSecret="123123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
